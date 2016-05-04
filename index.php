decc8ce02cddb3579bba6ea717cf49cf07ab1457
Wisertown <aaron.wise253@gmail.com>
2016-05-04 07:20:32 -0400

    added viewport and ipad responsive rules

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>The Best Hike Blog</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">-->
 
